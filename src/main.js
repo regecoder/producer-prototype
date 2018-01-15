f56835db7623282397e46bad099009c572d2ba14
regecoder <regecoder@gmail.com>
2018-01-15 22:42:17 +0100

    Fusionne le routeur dans main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+
 import Vue from 'vue';
+import Router from 'vue-router';
+
+import { routes } from '@/modules';
+// import App from './App';
 import App from './modules/app/components/App';
-import router from './router';
 
 Vue.config.productionTip = false;
+
+Vue.use(Router);
+
+const router = new Router({
+  routes
+});
 
 /* eslint-disable no-new */
 new Vue({
@@ -13,3 +23,12 @@ new Vue({
   template: '<App/>',
   components: { App }
 });
+
+// router.map({
+//   '/': {
+//     name: 'home',
+//     component: Vue.component('home', require('./components/Home'))
+//   }
+// });
+
+// router.start(App, '#app');
