eff50df541ec0159fdb43c1ed86e7be10274ca81
regecoder <regecoder@gmail.com>
2018-01-29 15:21:58 +0100

    Change la configuration auth0

--- src/modules/auth/config.js
+++ src/modules/auth/config.js
@@ -1,10 +1,9 @@
 import { config as coreConfig } from 'Core';
 
 const config = {
-  clientId: '3Z-MEzcmq_LDbj9L35SGVj_rrZPKbT_R',
+  clientId: '0WJOA82leNz5jbBWZ0vM8xyyH48AVqSr',
   domain: 'scenso.eu.auth0.com',
-  // audience: 'https://scenso.eu.auth0.com/userinfo',
-  audience: 'https://falcor.scenso/api/v1/',
+  audience: 'https://scenso.eu.auth0.com/userinfo',
   callbackUrl: `http://${coreConfig.domain}/auth/callback`,
   responseType: 'token id_token',
   scope: 'openid',
