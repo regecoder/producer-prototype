36162d71a21ad95d79e3713031d4f9eb24717473
regecoder <regecoder@gmail.com>
2018-01-30 17:57:15 +0100

    Restructure le dispatcher de la route racine

--- src/core/config.js
+++ src/core/config.js
@@ -1,6 +1,7 @@
 const envConfig = require(`./config.${process.env.NODE_ENV}`);
 
 const config = {
+  welcomeRoute: 'welcome',
   loginSuccessRoute: 'dashboard',
   loginErrorRoute: 'unauthorized',
   logoutRoute: 'welcome'
--- src/core/routes.js
+++ src/core/routes.js
@@ -1,13 +1,28 @@
-import Root from './components/root';
+import { service as authService } from 'Modules/auth';
+
+import config from './config';
 import Welcome from './components/welcome';
 import Unauthorized from './components/unauthorized';
 import NotFound from './components/not-found';
+
 
 export default [
   {
     path: '/',
     name: 'root',
-    component: Root
+    beforeEnter: (to, from, next) => {
+      if (authService.isAuthenticated) {
+        next({
+          name: config.loginSuccessRoute,
+          replace: true
+        });
+      } else {
+        next({
+          name: config.welcomeRoute,
+          replace: true
+        });
+      }
+    }
   },
   {
     path: '/welcome',
