144e3694893646407d181cd239cda08f49a10e08
regecoder <regecoder@gmail.com>
2018-02-08 11:27:15 +0100

    Integre eslint-plugin-vue

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,18 +2,21 @@
 
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
   parserOptions: {
+    parser: 'babel-eslint',
     sourceType: 'module'
   },
   env: {
     browser: true,
   },
-  extends: 'airbnb-base',
-  // required to lint *.vue files
-  plugins: [
-    'html'
+  extends: [
+    'airbnb-base',
+    'plugin:vue/recommended'
   ],
+  // required to lint *.vue files
+  // plugins: [
+  //   'html',
+  // ],
   // check if imports actually resolve
   settings: {
     'import/resolver': {
