a9e90c321704b1486623dbb9159fbd73deec90fa
regecoder <regecoder@gmail.com>
2018-02-13 14:45:53 +0100

    Ajoute les societes d'auteurs personnalisees

--- src/modules/videos/components/video-edit-step3.vue
+++ src/modules/videos/components/video-edit-step3.vue
@@ -4,14 +4,28 @@
       .form-panel
         .form-panel-title Sociétés d'auteurs
         .form-section
-          .form-field.form-field-checkbox(v-for="authorsSociety in authorsSocieties")
-            input(type="checkbox"
+          .form-field.form-field-checkbox(v-for="authorsSociety in defaultAuthorsSocieties")
+            input(
+              type="checkbox"
               :id="authorsSociety.name"
               :value="authorsSociety.name"
               v-model="model.authorsSocieties[authorsSociety.name]"
             )
             label(:for="authorsSociety.name") {{ authorsSociety.label }}
-            .tag {{ authorsSociety.tag }}
+            .percentage {{ authorsSociety.percentage }}
+            .unit %
+          .form-field.form-field-checkbox.form-field-checkbox-custom(
+            v-for="(authorsSociety, index) in customAuthorsSocieties"
+          )
+            input(
+              type="checkbox"
+              :value="getCustomAuthorsSocietyName(index)"
+              v-model="authorsSociety.checked"
+            )
+            input.input-label(type="text" v-model="authorsSociety.label")
+            input.input-percentage(type="text" v-model="authorsSociety.percentage")
+            .unit %
+          button(type="button" @click="addCustomAuthorsSociety()") Ajouter une société d'auteurs
       .form-panel.form-command-panel
         button(type="button" @click="preHandleNextStep()") Je passe à l'étape suivante
         router-link(:to="getPreviousStep()" replace) Je reviens à l'étape précédente
@@ -20,43 +34,58 @@
 <script>
 import videoEditMixin from 'Modules/videos/mixins/video-edit.mixin';
 
-const authorsSocieties = [
+const defaultAuthorsSocieties = [
   {
     name: 'privateCopy',
     label: 'Copie privée',
-    tag: '2%'
+    percentage: 2
   },
   {
     name: 'sacem',
     label: 'SACEM',
-    tag: '10%'
+    percentage: 10
   },
   {
     name: 'sacd',
     label: 'SACD',
-    tag: '5%'
+    percentage: 5
   },
   {
     name: 'scam',
     label: 'SCAM',
-    tag: '5%'
+    percentage: 5
   }
 ];
+
+const customAuthorsSocieties = [];
 
 export default {
   mixins: [
     videoEditMixin
   ],
+
   data() {
     return {
       step: {
         order: 3,
         storeKey: 'exploitation'
       },
-      authorsSocieties
+      defaultAuthorsSocieties,
+      customAuthorsSocieties
     };
   },
+
   methods: {
+    addCustomAuthorsSociety: function () {
+      customAuthorsSocieties.push({
+        // checked: true
+      });
+    },
+
+    getCustomAuthorsSocietyName: function (index) {
+      return `custom${index}`;
+    },
+
     preHandleNextStep: function () {
       // Supprime les sociétés d'auteurs non cochées (=false) du modèle
       deleteUnselectedAuthorsSocieties(this.model.authorsSocieties);
