3c0f03db5ca4968cdcc7d4d7312afbc76c1d76b8
regecoder <regecoder@gmail.com>
2018-03-16 14:40:28 +0100

    Ajoute le nom de domaine de production producer.polkatulk.com

--- src/core/config/env.production.js
+++ src/core/config/env.production.js
@@ -1,5 +1,9 @@
 module.exports = {
   protocol: 'https',
+  // Bucket S3
   // domain: 'scenso-producer-web.s3-website.eu-west-3.amazonaws.com'
-  domain: 'do5b6p9zvhgcd.cloudfront.net'
+  // Distribution Cloudfront
+  // domain: 'do5b6p9zvhgcd.cloudfront.net'
+  // Domaine polkatulk
+  domain: 'producer.polkatulk.com'
 };
